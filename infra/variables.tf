50c836b798a13f0e17b76b787c1575d9115b9a5b
guidssantos <guilhermedssantos1@gmail.com>
2024-07-13 16:35:42 -0300

    feat: security groups and variables

---
+++ infra/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "instance_type" {              
+    type = string
+    default = "t2.micro"  
+}
+
+variable "ami_id" {              
+    type = string
+    # default = ""  
+}
+
+variable "my_ip" {   
+    type = string
+}
+
+variable "key_name" {   
+    type = string
+}
+
+variable "securityGroup" {   
+    type = string
+}
+
+variable "name" {   
+    type = string
+}
+
+variable "production" {
+    type = bool
+}
+
