361a4b9bb37bb6ac2b417731e00a3ec2c7423b7e
guidssantos <guilhermedssantos1@gmail.com>
2024-07-14 11:35:09 -0300

    fix: new ports for grafana and prometheus

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -9,6 +9,27 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
+    from_port = 9090
+    to_port = 9090
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
