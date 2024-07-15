b85a1551ac66d23ea700d5503700bba80d166d3a
guidssantos <guilhermedssantos1@gmail.com>
2024-07-15 14:45:52 -0300

    fix: modify backend content

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,16 @@ terraform {
   }
 }
 
+terraform {
+    backend "s3" {
+      bucket = "terraform-state-devops-v4"
+      key = "terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "tfstate"
+        encrypt        = true
+    }
+}
+
 provider "aws" {
   region = var.aws_region
 }
