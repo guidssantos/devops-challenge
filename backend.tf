7e984dedd4590e99cabd7afcf5993130c8e893e4
guidssantos <guilhermedssantos1@gmail.com>
2024-07-15 15:31:28 -0300

    fix: test pipeline

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-devops-v4"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tfstate"
+    encrypt        = true
+  }
+}
--- infra/main.tf
+++ infra/main.tf
@@ -6,16 +6,7 @@ terraform {
       version = "2.1.0"
     }
   }
-}
 
-terraform {
-    backend "s3" {
-      bucket = "terraform-state-devops-v4"
-      key = "terraform.tfstate"
-      region = "us-east-1"
-      dynamodb_table = "tfstate"
-        encrypt        = true
-    }
 }
 
 provider "aws" {
