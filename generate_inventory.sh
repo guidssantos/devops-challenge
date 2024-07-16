2610a99ef3650aa34a44f7f5e8a5e7f4480b39d3
guidssantos <guilhermedssantos1@gmail.com>
2024-07-16 16:00:04 -0300

    fix: new test

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -2,7 +2,7 @@
 
 instance_ip=$(terraform output -raw IP)
 
-cat <<EOL > inventory.ini
+cat <<EOL > ansible/inventory.ini
 [server]
 ec2-instance ansible_host="$instance_ip" ansible_user=ubuntu  ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOL
